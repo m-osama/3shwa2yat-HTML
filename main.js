2547416e5aae2b21d9552bdfb5c736dcf1e907b7
m-osama <mohammedosama@gmail.com>
2019-03-17 14:07:11 +0200

    jsUpdata

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var overlay = document.getElementById('overlay');
-    var closeMenu = document.getElementById('close-menu');
+let overlay = document.getElementById('overlay');
+    let closeMenu = document.getElementById('close-menu');
+    let close = document.getElementById('close');
 
     document.getElementById('open-menu').addEventListener('click', function(){
         overlay.classList.add('show-menu');
@@ -9,4 +10,7 @@ var overlay = document.getElementById('overlay');
         overlay.classList.remove('show-menu');
     });
 
+    document.getElementById('close').addEventListener('click', function(){
+        overlay.classList.remove('show-menu');
+    });
 
